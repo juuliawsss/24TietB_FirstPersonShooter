b4ee33b03c64da2a56594b5be7493bf25d84df0c
juuliawsss <juuliawylipelkonen@gmail.com>
2025-03-13 10:31:06 +0200

    granade

--- Assets/Juulia/JY_Scripts/JY_Grenade.cs
+++ Assets/Juulia/JY_Scripts/JY_Grenade.cs
@@ -1,16 +1,79 @@
 using UnityEngine;
 
 public class JY_Grenade : MonoBehaviour
+
 {
+    public float delay = 3f;
+
+    public float blastRadius = 5f;
+
+    public float explosionForce = 700f;
+
+    public float damageAmount = 50f;
+
+    public LayerMask damageableLayer;
+
+    private bool hasExploded = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        StartCoroutine(ExplodeAfterDelay());
+    }
+    IEnumerator ExplodeAfterDelay()
+    {
+
+        yield return new WaitForSeconds(delay);
+
+        Explode();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void Explode()
+
     {
-        
+
+        if (hasExploded) return;
+
+
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius, damageableLayer);
+
+        foreach (Collider nearbyObject in colliders)
+
+        {
+
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+
+            {
+
+                rb.AddExplosionForce(explosionForce, transform.position, blastRadius);
+
+            }
+
+
+
+            IDamageable damageable = nearbyObject.GetComponent<IDamageable>();
+
+            if (damageable != null)
+
+            {
+
+                damageable.TakeDamage(damageAmount);
+
+            }
+
+        }
+
+
+
+        hasExploded = true;
+
+        Destroy(gameObject);
+
     }
 }
